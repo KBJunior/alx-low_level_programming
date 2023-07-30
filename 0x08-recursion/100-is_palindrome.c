d041fdc19427a07ec9044e29202f645f9d57bcaa
KBJunior <kbjunior6@gmail.com>
2023-07-30 06:08:04 -0700

    recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -8,8 +8,8 @@
  */
 int is_palindrome(char *s)
 {
-  int len = _strlen_recursion(s);
-  return check_pal(s, 0, len - 1);
+ int len = _strlen_recursion(s);
+ return check_pal(s, 0, len - 1);
 }
 
 /**
@@ -20,9 +20,9 @@ int is_palindrome(char *s)
  */
 int _strlen_recursion(char *s)
 {
-  if (*s == '\0')
-    return 0;
-  return 1 + _strlen_recursion(s + 1);
+ if (*s == '\0')
+  return (0);
+ return (1 + _strlen_recursion(s + 1));
 }
 
 /**
@@ -35,11 +35,12 @@ int _strlen_recursion(char *s)
  */
 int check_pal(char *s, int start, int end)
 {
-  if (start >= end)
-    return 1;
+ if (start >= end)
+  return (1);
 
-  if (s[start] != s[end])
-    return 0;
+ if (s[start] != s[end])
+  return (0);
 
-  return check_pal(s, start + 1, end - 1);
+ return check_pal(s, start + 1, end - 1);
 }
+
